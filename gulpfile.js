937f3622f69ef8e17b9830d2d6678cf9e6bdb68b
Gabriel Florit <gabrielflorit@gmail.com>
2015-09-19 23:27:11 -0400

    Add gulp and browser sync integration.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp        = require('gulp');
+var shell       = require('gulp-shell');
+var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence');
+
+gulp.task('watch', function() {
+	gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('build', shell.task(['jekyll build --watch']));
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: '_site/'
+		},
+		notify: false,
+		ghostMode: false
+	});
+});
+
+gulp.task('default', function() {
+
+	runSequence(
+		'watch',
+		'browser-sync',
+		'build'
+	);
+
+});
